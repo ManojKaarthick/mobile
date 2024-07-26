9f27b9095364f755886768f2cc966dc02b1d9225
ManojKaarthick <manojkaarthick2@gmail.com>
2024-07-26 12:47:30 +0530

    helllppp

---
+++ src/test/java/Junit/Baseclass.java
@@ -0,0 +1,54 @@
+package Junit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class Baseclass {
+     public static WebDriver driver;
+
+    public static void browserlaunch() {
+
+        driver = new ChromeDriver();
+    }
+    public static void url(String SE) {
+        driver.get(SE);
+        driver.manage().window().maximize();
+    }
+    public static  void sendkeys(WebElement w,String S){
+        w.sendKeys(S);
+    }
+
+    public static void click(WebElement C){
+
+        C.click();
+    }
+
+    public static void clear(WebElement clear){
+
+        clear.clear();
+    }
+    public static  void selectByDropDown(WebElement element, String value) {
+        Select dropdown = new Select(element);
+        dropdown.selectByValue(value);
+    }
+
+    public  static void browserclose(){
+
+        driver.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ src/test/java/Junit/Runner.java
@@ -0,0 +1,12 @@
+package Junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(login.class)
+
+public class Runner extends Baseclass {
+
+
+}
